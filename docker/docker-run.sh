c970188a6feb3e54995a7ee7e724f29f8c2a5aad
ictus4u <47392003+ictus4u@users.noreply.github.com>
2021-06-30 01:20:09 -0400

    #1 Add basic docker run script
    With cmake and suport for ccache

---
+++ docker/docker-run.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+script_folder=$(realpath $(dirname $0))
+
+image_name=lfs-build
+container_name=lfs-built
+
+docker run --rm -it \
+  --name ${container_name} \
+  -e CCACHE_DIR=/ccache \
+  -v "${script_folder}/../:/project" \
+  -v "${script_folder}/../volumes/ccache:/ccache" \
+  -d ${image_name} \
+  "mkdir -p ./build \
+  && cd ./build \
+  && cmake .. -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
+  && tail -f /dev/null"
