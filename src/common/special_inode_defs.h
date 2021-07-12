721efb0d8f94359051e573e2c08840a6dd97ae00
ictus4u <47392003+ictus4u@users.noreply.github.com>
2021-07-12 07:24:54 -0400

    mount: Define a .baldor special file
    Defines constants associated to the special file name and inode for a new special file
    By reading this file, it will be possible to get data about a contributor

--- src/common/special_inode_defs.h
+++ src/common/special_inode_defs.h
@@ -32,9 +32,11 @@
 #define SPECIAL_INODE_META_TRASH      (SPECIAL_INODE_BASE + 0x5U)
 #define SPECIAL_INODE_META_UNDEL      (SPECIAL_INODE_BASE + 0x6U)
 #define SPECIAL_INODE_META_RESERVED   (SPECIAL_INODE_BASE + 0x7U)
+#define SPECIAL_INODE_BALDOR          (SPECIAL_INODE_BASE + 0xBU)
 
 #define SPECIAL_FILE_NAME_MASTERINFO      ".masterinfo"
 #define SPECIAL_FILE_NAME_STATS           ".stats"
+#define SPECIAL_FILE_NAME_BALDOR          ".baldor"
 #define SPECIAL_FILE_NAME_OPLOG           ".oplog"
 #define SPECIAL_FILE_NAME_OPHISTORY       ".ophistory"
 #define SPECIAL_FILE_NAME_TWEAKS          ".lizardfs_tweaks"
